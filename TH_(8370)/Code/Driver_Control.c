aa64e6c1552e8be4f1e32555b458cad647a126f5
Michael <michaellocicero@Michael-LoCicero.local>
2015-11-10 15:12:53 -0500

    Added Automatous
    -

--- TH_(8370)/Code/Driver_Control.c
+++ TH_(8370)/Code/Driver_Control.c
@@ -49,16 +49,31 @@ void pre_auton()
 
 task autonomous()
 {
-  // .....................................................................................
-  {
-  	motor(leftDrive) = 127;
-  motor(rightDrive) = 127;
-  wait1Msec(5000);
-	}
-	 wait1Msec(5000);
-  // .....................................................................................
+	wait1Msec(2000);
+	
+	int threshold = 505;
+	
+	clearTimer(T1)
+		
+		while(time1[T1] < 10000)
+	{
+		if(SensorValue(lineTracker) < threshold)
+		{	
+			motor[leftMotor] = 63;
+			motor[rightMotor] = 0;
+		}
+		wait(5)
+		{
+			motor[leftMotor]  = 0;
+			motor[rightMotor]  = 63;
+		}
 
-	AutonomousCodePlaceholderForTesting();  // Remove this function call once you have "real" code.
+		else
+		{
+			wait(999999999999999999)
+		}
+	}
+	AutonomousCodePlaceholderForTesting();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
